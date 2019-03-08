12245f75ec9c3c54210b133144138b11ebe44536
JM <jdreher15@gmail.com>
2019-03-08 11:17:47 -0800

    form model and material and icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AvantHeaderComponent } from './avant-header/avant-header.component';
@@ -14,7 +15,8 @@ import { AvantFooterComponent } from './avant-footer/avant-footer.component';
     AvantFooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/avant-profile/avant-profile.component.ts
+++ src/app/avant-profile/avant-profile.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
+import { UserProfile } from './userModel';
 
 @Component({
   selector: 'app-avant-profile',
   templateUrl: './avant-profile.component.html',
   styleUrls: ['./avant-profile.component.css']
 })
+
 export class AvantProfileComponent implements OnInit {
+  model: UserProfile;
 
   constructor(
     @Inject(DOCUMENT) private document: Document
@@ -16,20 +19,23 @@ export class AvantProfileComponent implements OnInit {
     document.getElementById('defaultOpen').click();
   }
 
+  onSubmit() {
+    const reqBody = new UserProfile(
+      this.model.firstName,
+      this.model.middleName,
+      this.model.lastName);
+  }
+
   showTabContent(e, tabType) {
-    // Declare all variables
     let i, tabcontent, tablinks;
-    // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName('tabcontent');
     for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = 'none';
     }
-    // Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName('tablinks');
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(' active', '');
     }
-    // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(tabType).style.display = 'block';
     e.currentTarget.className += ' active';
   }
